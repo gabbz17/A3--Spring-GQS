2b3ab0f70f41249cc8b693b78965615449672064
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-11-16 19:57:58 -0300

    test: create a test findAll of JUnit

--- src/test/java/com/example/demo/JUnit/PeopleServiceTest.java
+++ src/test/java/com/example/demo/JUnit/PeopleServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.JUnit;
 
 import com.example.demo.Service.PeopleService;
 import com.example.demo.entity.People;
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class PeopleServiceTest {
@@ -35,5 +36,19 @@ public class PeopleServiceTest {
         People create = service.create(people);
 
         Assertions.assertNotNull(create);
+    }
+
+    @Test
+    void findAllUserTest() {
+
+        People gabriel = new People(null, "Gabriel", "gabriel@gmail.com", "11111111111", BigDecimal.valueOf(1000));
+        People ana = new People(null, "Ana", "ana@gmail.com", "22222222222", BigDecimal.valueOf(1000));
+
+        Mockito.when(repository.findAll()).thenReturn(List.of(gabriel, ana));
+
+        List<People> findAll = service.findAll();
+
+        Assertions.assertEquals("Gabriel", findAll.getFirst().getName());
+        Assertions.assertNotNull(findAll);
     }
 }
