d0516abbfdfb076fae5cb311f834f0f8e9c286b0
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-11-28 20:07:36 -0300

    test: create a test assured

--- src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
+++ src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
@@ -1,4 +1,15 @@
 package com.example.demo.restAssured;
 
+import org.junit.jupiter.api.Test;
+
 public class PeopleServiceTest {
+
+    @Test
+    void createPeopleTest() {
+        String baseURI = "http://localhost";
+        int port = 8080;
+        String basePath = "/api/v1/people";
+
+
+    }
 }
