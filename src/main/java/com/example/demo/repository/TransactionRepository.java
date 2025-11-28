d1ff54568f92ca9c2b48e36dfaf2e26c0f92166e
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-11-28 20:06:02 -0300

    feat: create a Transaction repository

--- src/main/java/com/example/demo/repository/TransactionRepository.java
+++ src/main/java/com/example/demo/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public interface TransactionRepository {
+import com.example.demo.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 }
