5ae7572bd57648378958fec153e2153ba0acb478
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-11-30 14:20:37 -0300

    refactor: add a dependency of transaction class

--- src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
+++ src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
@@ -1,15 +1,45 @@
 package com.example.demo.restAssured;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-public class PeopleServiceTest {
+import static org.assertj.core.api.BDDAssumptions.given;
+import static org.hamcrest.Matchers.*;
+import static sun.security.util.KnownOIDs.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PeopleControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
 
     @Test
-    void createPeopleTest() {
-        String baseURI = "http://localhost";
-        int port = 8080;
-        String basePath = "/api/v1/people";
+    void shouldCreatePersonSuccessfully() {
+        String jsonBody = """
+        {
+            "name": "Gabriel",
+            "email": "gabriel@gmail.com",
+            "cpf": "11111111111",
+            "balance": 1000.0
+        }
+        """;
 
-
+        given()
+                .contentType(ContentType.JSON)
+                .body(jsonBody)
+                .when()
+                .post("/people")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Gabriel"))
+                .body("email", equalTo("gabriel@gmail.com"))
+                .body("cpf", equalTo("11111111111"));
     }
 }
