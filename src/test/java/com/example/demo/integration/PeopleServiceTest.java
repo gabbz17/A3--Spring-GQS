42955999b7cf5ae7796bbfc07783e3f53aa2e079
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-12-03 20:56:51 -0300

    test: create a integration test of findById

--- src/test/java/com/example/demo/integration/PeopleServiceTest.java
+++ src/test/java/com/example/demo/integration/PeopleServiceTest.java
@@ -4,6 +4,7 @@ import com.example.demo.Service.PeopleService;
 import com.example.demo.entity.People;
 import com.example.demo.repository.PeopleRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.List;
 //menciona que esta classe é um ambiente para teste de integracao
 @SpringBootTest
 @Transactional
+@Slf4j
 public class PeopleServiceTest {
 
     //injeta dependencias
@@ -23,14 +25,15 @@ public class PeopleServiceTest {
     
     @Autowired
     PeopleRepository repository;
+
+    People gabriel = new People(null, "Gabriel", "gabriel@gmail.com", "11111111111", BigDecimal.valueOf(1000));
     
     @Test
     void createUserTest() {
         //fluxo de teste: instancia do objeto -> chamada do metodo de teste -> verificacao com o JUnit = assert ->
         // chama metodo na camada mais proxima do banco e valida os dados
-        People people = new People(null, "Gabriel", "gabriel@gmail.com", "11111111111", BigDecimal.valueOf(1000));
 
-        People create = service.create(people);
+        People create = service.create(gabriel);
         Assertions.assertEquals("Gabriel", create.getName());
 
         People find = repository.findById(1L).get();
@@ -40,16 +43,26 @@ public class PeopleServiceTest {
     @Test
     void findAllUserTest() {
 
-        People gabriel = new People(null, "Gabriel", "gabriel@gmail.com", "11111111111", BigDecimal.valueOf(1000));
-
-
         People create = service.create(gabriel);
         Assertions.assertEquals("11111111111", create.getCpf());
 
         List<People> findAll = repository.findAll();
         Assertions.assertNotNull(findAll);
         Assertions.assertEquals("Gabriel", findAll.getFirst().getName());
+    }
+
+    @Test
+    void findByIdTest() {
+
+        People create = service.create(gabriel);
+        Assertions.assertEquals("11111111111", create.getCpf());
+        log.info("Usuario cadastrado: " + create);
+
+        People findById = service.findById(15L);
+        Assertions.assertNotNull(findById);
 
 
     }
+
+
 }
