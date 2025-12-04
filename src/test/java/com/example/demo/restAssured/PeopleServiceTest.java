c0c18d06bb6b03ea7258a0199a7706d36e6548fb
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-12-03 22:02:45 -0300

    test: create a test assure of all methods

--- src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
+++ src/test/java/com/example/demo/restAssured/PeopleServiceTest.java
@@ -1,45 +1,98 @@
 package com.example.demo.restAssured;
 
+import com.example.demo.entity.People;
+import com.example.demo.web.dto.RequestNameUpdate;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.assertj.core.api.BDDAssumptions.given;
+import java.math.BigDecimal;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
-import static sun.security.util.KnownOIDs.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PeopleControllerIntegrationTest {
+class PeopleControllerTest {
 
     @LocalServerPort
     private int port;
 
     @BeforeEach
-    void setup() {
+    void setUp() {
         RestAssured.port = port;
+        RestAssured.basePath = "/api/v1/people";
     }
 
+    People people = new People(24L, "Carlos", "carlos@gmail.com", "11111111111", BigDecimal.valueOf(1000));
+
     @Test
-    void shouldCreatePersonSuccessfully() {
-        String jsonBody = """
-        {
-            "name": "Gabriel",
-            "email": "gabriel@gmail.com",
-            "cpf": "11111111111",
-            "balance": 1000.0
-        }
-        """;
+    void createPeopleTest() {
 
         given()
                 .contentType(ContentType.JSON)
-                .body(jsonBody)
+                .body(people)
                 .when()
-                .post("/people")
+                .post()
                 .then()
                 .statusCode(201)
-                .body("name", equalTo("Gabriel"))
-                .body("email", equalTo("gabriel@gmail.com"))
-                .body("cpf", equalTo("11111111111"));
+                .body("name", equalTo("Carlos Drummond"));
     }
+
+    @Test
+    void findByIdTest() {
+
+        given()
+                .pathParam("id", people.getNumberAccount())
+                .when()
+                .get("/id/{id}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Joseph"));
+
+    }
+
+    @Test
+    void findAllTest() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0))
+                .body("[0].name", notNullValue());
+    }
+
+    @Test
+    void updateNameTest() {
+
+        RequestNameUpdate updateDto = new RequestNameUpdate("Joseph");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(updateDto)
+                .pathParam("id", people.getNumberAccount())
+                .when()
+                .patch("/id/{id}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Joseph"));
+    }
+
+    @Test
+    void deleteByIdTest() {
+
+        given()
+                .pathParam("id", people.getNumberAccount())
+                .when()
+                .delete("/delete/{id}")
+                .then()
+                .statusCode(204); // No Content
+    }
+
 }
